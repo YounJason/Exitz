a035a08edee6cd964d42176f869cc749ead9f537
YounJason <jason@yjsn.me>
2026-08-09 00:09:53 +0900

    Cloudflare Migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   output: 'static',
   site: 'https://exitz.me',
-  adapter: netlify(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: { enabled: true },
+  }),
+  vite: {
+    ssr: {
+      external: ['@resvg/resvg-js'],
+    },
+  },
   markdown: {
     rehypePlugins: [
       [
